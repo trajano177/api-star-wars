357008b050e4a5cc82bc20b3a478783e7a8e7fcc
Matheus Trajano <matheustrajanotel@gmail.com>
2023-01-20 22:30:11 -0300

    att

--- src/Card/card.tsx
+++ src/Card/card.tsx
@@ -1,32 +1,20 @@
-import styles from './card.module.css';
-import { api } from '../App';
-import * as Dialog from '@radix-ui/react-dialog';
-
+import styles from "./card.module.css";
+import { api } from "../App";
+import * as Dialog from "@radix-ui/react-dialog";
 
 interface cardProps {
-    person: api
-    idPerson: number
+  person: api;
+  idPerson: number;
 }
 
 export function Card({ person, idPerson }: cardProps) {
-    return (
-        <>
-            <Dialog.Root>
-                <Dialog.Trigger asChild>
-
-                    <div className={styles.cards}>
-                        <img src={`./person/${idPerson}.png`} />
-                        <h3 style={{ color: 'white' }}>{person.name}</h3>
-                        <p style={{ color: 'white' }}>{person.birth_year}</p>
-
-                    </div>
-                </Dialog.Trigger>
-            </Dialog.Root>
-
-
-
-
-        </>
-
-    )
+  return (
+    <>
+      <div className={styles.cards}>
+        <img src={`./person/${idPerson}.png`} />
+        <h3>{person.name}</h3>
+        <p>{person.birth_year}</p>
+      </div>
+    </>
+  );
 }
